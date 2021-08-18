8d951f98981109cbe061ce5d763557d76c43fe00
prathish-code <88712212+Prathish-codes@users.noreply.github.com>
2021-08-18 23:27:11 +0530

    updated:js imported

---
+++ app.js
@@ -0,0 +1,8 @@
+////******click event */
+//btnTranslate.addEventListener {"click",function clickEvenHandler(),
+  // console.log("clicked")
+//}
+
+var btnTranslate =document.querySelector("#btn-translate")
+
+console.log(btnTranslate)
