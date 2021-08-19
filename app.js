dfd343f890622fc21cb51c8b87503cc981234083
prathish-code <88712212+Prathish-codes@users.noreply.github.com>
2021-08-19 20:14:51 +0530

    updated:api translator

--- app.js
+++ app.js
@@ -3,11 +3,34 @@ var txtInput =document.querySelector("#txt-input")
 var outputDiv= document.querySelector("#output")
 
 
+var serverURL =("https://api.funtranslations.com/translate/minion.json")
 
-function clickHander(){
-    outputDiv.innerText="oh oh"+txtInput.value;
-  
-};
+
+
+function getTranslationURL(text){
+    return serverURL +"?"+"text="+text
+}
+
+
+function errorHandler(text){
+    return serverURL("erorr occured" ,error);
+}
+
+
+function clickHander() {
+    var inputText=txtInput.value;///taking input
+
+    //calling server function for procssing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+         .then(json =>{
+          var translatedText = json.contents.translated;
+          outputDiv.innerText=translatedText;
+         })
+
+        .catch(errorHandler)  
+             
+};                
 
 ////******click event */
 btnTranslate.addEventListener ("click" ,  clickHander)
