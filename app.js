9eb4d49eafed8998df08171cf93a1b1837d7e8ac
prathish-code <88712212+Prathish-codes@users.noreply.github.com>
2021-08-19 10:08:42 +0530

    updated:text input

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var btnTranslate =document.querySelector("#btn-translate")
+var txtInput =document.querySelector("#txt-input")
+
+
+console.log(txtInput)
 
 function clickHander(){
     console.log("clicked")
+    //console.log("input", txtInput.value)
 };
 
 ////******click event */
